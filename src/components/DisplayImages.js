4b9e764b21f818374b1696716810793250c78173
Joshua Ramnaraine <joshuaramnaraine14@gmail.com>
2022-01-17 17:52:03 -0500

    Modify

--- src/components/DisplayImages.js
+++ src/components/DisplayImages.js
@@ -13,6 +13,23 @@ export default function DisplayImages(props) {
         }
         return false;
     }
+    const checkTags = (img) => {
+        if (img.data[0].keywords.length != 0) {
+            displayTags(img);
+        }
+    };
+    const displayTags = (img) => {
+        return (
+            <div className='d-flex flex-row'>
+                <h6 className='m-1'><span className="badge badge-pill badge-dark">Tags</span></h6>
+                <div className='d-flex flex-row flex-wrap'>
+                    {img.data[0].keywords.map((tags) => (
+                        <h6 className='m-1 badgeColor badge badge-pill text-break'>{tags}</h6>
+                    ))}
+                </div>
+            </div>
+        );
+    }
     return (
         <div className='mx-auto w-75'>
             <div className='row'>
@@ -33,14 +50,15 @@ export default function DisplayImages(props) {
                                 onClickBtn={() => { props.onClickLike(img) }}
                             />
                         </div>
-                        <div className='d-flex flex-row'>
+                        {checkTags(img)}
+                        {/* <div className='d-flex flex-row'>
                             <h6 className='m-1'><span className="badge badge-pill badge-dark">Tags</span></h6>
                             <div className='d-flex flex-row flex-wrap'>
                                 {img.data[0].keywords.map((tags) =>(
                                     <h6 className='m-1 badgeColor badge badge-pill text-break'>{tags}</h6>
                                 ))}
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 ))}
             </div>
